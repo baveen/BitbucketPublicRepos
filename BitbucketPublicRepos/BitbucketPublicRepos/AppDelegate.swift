5032ddbf42425f32128447ff398835f3f4866b99
Baveendran Nagendran <baveen0704@gmail.com>
2021-08-05 12:09:25 +0800

    base project created and UIScene related files removed

---
+++ BitbucketPublicRepos/BitbucketPublicRepos/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  BitbucketPublicRepos
+//
+//  Created by Baveendran Nagendran on 2021-08-05.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
